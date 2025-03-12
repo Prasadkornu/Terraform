12565eb8441e0bc8d27faa77736b4f14c6a00cf5
Prasad Kornu <pkornu@zetaglobal.com>
2025-03-12 16:46:09 +0530

    add

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "eu-north-1"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
+  ami           = "ami-0c2e61fdcb5495691" 
+  instance_type = "t3.micro"
   tags = {
       Name = "TF-Instance"
   }
